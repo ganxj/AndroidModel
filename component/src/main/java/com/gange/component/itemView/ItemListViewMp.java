ef42e07f8f1b418ddc02e791beb24813f71cdb67
gange <749918694@qq.com>
2019-05-11 23:52:55 +0800

    列表报错修复

--- component/src/main/java/com/gange/component/itemView/ItemListViewMp.java
+++ component/src/main/java/com/gange/component/itemView/ItemListViewMp.java
@@ -2,7 +2,11 @@ package com.gange.component.itemView;
 
 import android.app.Application;
 import android.databinding.ObservableArrayList;
+import android.databinding.ObservableField;
+import android.databinding.ObservableList;
 import android.support.annotation.NonNull;
+
+import com.gange.component.R;
 
 import java.util.List;
 
@@ -11,10 +15,12 @@ import me.goldze.mvvmhabit.base.BaseViewModel;
 public class ItemListViewMp extends BaseViewModel {
 
     public ObservableArrayList<ItemView1Mp> list = new ObservableArrayList<>();
+    public ObservableField<Integer> view = new ObservableField<>(R.layout.item_grid_view1);
 
 
     public ItemListViewMp(@NonNull Application application , ObservableArrayList<ItemView1Mp> list) {
         super(application);
         this.list = list;
     }
+
 }
